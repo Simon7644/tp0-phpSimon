a051e15cfe553397a2833b8aefdccf3d667a72bb
Simon7644 <simonbonnet76@gmail.com>
2022-02-18 16:07:53 +0100

    Exercice 5 paramètres dans l'url

---
+++ Exercice 5/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tp0php</title>
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    <!-- Style.css -->
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<a href="lien.php?nom=Bonnet&amp;prenom=Simon">Lien vers mon nom et prénom</a>
+<a href="lien.php?age=26 ans">Lien vers mon age</a>
+<a href="lien.php?email=simonbonnet76@gmail.com">Lien vers mon mail</a>
+
+
+
+
+
+</body>
+</html>
