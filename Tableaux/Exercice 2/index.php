f5590b6fdfb2b16e94fada5ebb90024effda3eac
Simon7644 <simonbonnet76@gmail.com>
2022-02-25 10:58:52 +0100

    Tableaux Exercice 2 Multidimensionnel 2

---
+++ Tableaux/Exercice 2/index.php
@@ -0,0 +1,7 @@
+<?php
+$tab=array(
+    "BONNET"=>array("Prenom"=>"Simon", "Ville"=>"La roche sur yon", "Age"=>26),
+    "HEDGEHOG"=>array("Prenom"=>"Sonic", "Ville"=>"Green hills", "Age"=>15));
+
+    print_r ($tab);
+    ?>
